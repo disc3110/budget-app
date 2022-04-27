d0900e4cff948133dca6cd509da64a3fff6fd3b6
Diego Solis <disc3110@gmail.com>
2022-04-26 19:29:10 -0500

    Add transactions page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,6 +10,8 @@ class GroupsController < ApplicationController
 
   # GET /groups/1 or /groups/1.json
   def show
+    @group = Group.find(params[:id])
+    @deals = @group.deals.order(created_at: :desc)
   end
 
   # GET /groups/new
